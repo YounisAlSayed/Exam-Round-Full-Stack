ca92b2476732aaf5303e3340c894fbd30c183105
Younis Al-Sayed <yuns20055@gmail.com>
2026-08-24 08:30:17 +0300

    Adding missing parts of the system

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,7 @@ define('BASE_PATH', dirname($_SERVER['SCRIPT_NAME']));
 use Dotenv\Dotenv;
 use App\Utils\Cors;
 use App\Routes\Router;
-
+$_SESSION['user'] = ['id' => 1, 'role' => 'teacher'];
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
