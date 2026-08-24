0c7331f3c6f18266d571d9cda4b530d218885a5c
Younis Al-Sayed <yuns20055@gmail.com>
2026-08-24 08:32:18 +0300

    Missing parts of the system

---
+++ itc.sql
@@ -0,0 +1,546 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 24, 2026 at 07:27 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `itc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attempts`
+--
+
+CREATE TABLE `attempts` (
+  `id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `started_at` datetime NOT NULL,
+  `submitted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choices`
+--
+
+CREATE TABLE `choices` (
+  `id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `choice_text` varchar(255) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT 0,
+  `one_correct` int(11) GENERATED ALWAYS AS (if(`is_correct`,`question_id`,NULL)) STORED
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `choices`
+--
+
+INSERT INTO `choices` (`id`, `question_id`, `choice_text`, `is_correct`) VALUES
+(1, 1, 'Structured Query Language', 1),
+(2, 1, 'Simple Question Logic', 0),
+(3, 1, 'Sequential Query List', 0),
+(4, 2, 'ch 1', 0),
+(5, 2, 'ch2', 1),
+(6, 2, 'ch 3', 0),
+(7, 3, 'ch 1', 0),
+(8, 3, 'ch2', 1),
+(9, 3, 'ch 3', 0),
+(10, 4, 'ch 1', 0),
+(11, 4, 'ch2', 1),
+(12, 4, 'ch 3', 0),
+(13, 5, 'ch 1', 0),
+(14, 5, 'ch2', 1),
+(15, 5, 'ch 3', 0),
+(16, 6, 'ch 1', 0),
+(17, 6, 'ch2', 1),
+(18, 6, 'ch 3', 0),
+(19, 7, 'ch 1', 0),
+(20, 7, 'ch2', 1),
+(21, 7, 'ch 3', 0),
+(22, 8, 'ch 1', 0),
+(23, 8, 'ch2', 1),
+(24, 8, 'ch 3', 0),
+(25, 9, 'ch 1', 0),
+(26, 9, 'ch2', 1),
+(27, 9, 'ch 3', 0),
+(28, 10, 'ch 1', 0),
+(29, 10, 'ch2', 1),
+(30, 10, 'ch 3', 0),
+(31, 11, 'ch 1', 0),
+(32, 11, 'ch 2', 1),
+(33, 11, 'ch 3', 0),
+(34, 12, 'ch 1', 0),
+(35, 12, 'ch 2', 1),
+(36, 12, 'ch 3', 0),
+(37, 13, 'ch 1', 0),
+(38, 13, 'ch 2', 1),
+(39, 13, 'ch 3', 0),
+(40, 14, 'ch 1', 0),
+(41, 14, 'ch 2', 1),
+(42, 14, 'ch 3', 0),
+(43, 15, 'ch 1', 0),
+(44, 15, 'ch 2', 1),
+(45, 15, 'ch 3', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `next_exam_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `name`, `next_exam_id`) VALUES
+(1, 'Intro to Databases', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrolment`
+--
+
+CREATE TABLE `enrolment` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `enrolment`
+--
+
+INSERT INTO `enrolment` (`id`, `student_id`, `course_id`) VALUES
+(1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+CREATE TABLE `exams` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `courses_id` int(11) NOT NULL,
+  `status` enum('not_ready','ready','in_progress','completed') NOT NULL DEFAULT 'not_ready',
+  `total_marks` int(11) NOT NULL CHECK (`total_marks` > 0 and `total_marks` <= 100),
+  `teacher_id` int(11) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `randomize_order` tinyint(1) NOT NULL DEFAULT 0
+) ;
+
+--
+-- Dumping data for table `exams`
+--
+
+INSERT INTO `exams` (`id`, `title`, `courses_id`, `status`, `total_marks`, `teacher_id`, `start_date`, `end_date`, `randomize_order`) VALUES
+(1, 'Midterm', 1, 'ready', 100, 1, '2026-09-01 09:00:00', '2026-09-01 10:00:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_questions`
+--
+
+CREATE TABLE `exam_questions` (
+  `id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `question_mark` double NOT NULL
+) ;
+
+--
+-- Dumping data for table `exam_questions`
+--
+
+INSERT INTO `exam_questions` (`id`, `exam_id`, `question_id`, `question_mark`) VALUES
+(1, 1, 1, 10),
+(2, 1, 2, 3),
+(3, 1, 3, 3),
+(4, 1, 4, 3),
+(5, 1, 5, 3),
+(6, 1, 6, 3),
+(7, 1, 7, 3),
+(8, 1, 8, 3),
+(9, 1, 9, 3),
+(10, 1, 10, 3),
+(11, 1, 11, 3),
+(12, 1, 12, 3),
+(13, 1, 13, 3),
+(14, 1, 14, 3),
+(15, 1, 15, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `marks`
+--
+
+CREATE TABLE `marks` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `student_mark` int(11) NOT NULL CHECK (`student_mark` >= 0 and `student_mark` <= 100)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `question` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `course_id`, `question`) VALUES
+(1, 1, 'What does SQL stand for?'),
+(2, 1, 'testing'),
+(3, 1, 'testing'),
+(4, 1, 'testing'),
+(5, 1, 'testing'),
+(6, 1, 'testing'),
+(7, 1, 'testing'),
+(8, 1, 'testing'),
+(9, 1, 'testing'),
+(10, 1, 'testing'),
+(11, 1, 'testing'),
+(12, 1, 'testing'),
+(13, 1, 'testing'),
+(14, 1, 'testing'),
+(15, 1, 'testing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_answers`
+--
+
+CREATE TABLE `student_answers` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `selected_choice_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_courses`
+--
+
+CREATE TABLE `teacher_courses` (
+  `id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teacher_courses`
+--
+
+INSERT INTO `teacher_courses` (`id`, `teacher_id`, `course_id`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role` enum('student','teacher') NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `email`, `password`, `first_name`, `last_name`, `image`) VALUES
+(1, 'teacher', 'teacher@test.com', 'PASTE_HASH_HERE', 'Jane', 'Smith', NULL),
+(2, 'student', 'student@test.com', 'PASTE_HASH_HERE', 'John', 'Doe', NULL),
+(4, 'student', 'customer1@example.com', '$2y$10$bE/3F8lfDu0kzdvfnNt7F.QRg3KBxarK2.E1Jn6h9PJKSQaQ/uL8m', 'test', 'test', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attempts`
+--
+ALTER TABLE `attempts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `exam_id` (`exam_id`,`student_id`),
+  ADD UNIQUE KEY `uq_exam_student` (`exam_id`,`student_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `choices`
+--
+ALTER TABLE `choices`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`,`question_id`),
+  ADD UNIQUE KEY `one_correct` (`one_correct`),
+  ADD KEY `question_id` (`question_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `fk_active_exam` (`next_exam_id`);
+
+--
+-- Indexes for table `enrolment`
+--
+ALTER TABLE `enrolment`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id` (`student_id`,`course_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `courses_id` (`courses_id`);
+
+--
+-- Indexes for table `exam_questions`
+--
+ALTER TABLE `exam_questions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `exam_id` (`exam_id`,`question_id`),
+  ADD KEY `question_id` (`question_id`);
+
+--
+-- Indexes for table `marks`
+--
+ALTER TABLE `marks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id` (`student_id`,`exam_id`),
+  ADD KEY `exam_id` (`exam_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `student_answers`
+--
+ALTER TABLE `student_answers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id` (`student_id`,`exam_id`,`question_id`),
+  ADD KEY `exam_id` (`exam_id`,`student_id`),
+  ADD KEY `exam_id_2` (`exam_id`,`question_id`),
+  ADD KEY `selected_choice_id` (`selected_choice_id`,`question_id`);
+
+--
+-- Indexes for table `teacher_courses`
+--
+ALTER TABLE `teacher_courses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `teacher_id` (`teacher_id`,`course_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attempts`
+--
+ALTER TABLE `attempts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `choices`
+--
+ALTER TABLE `choices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `enrolment`
+--
+ALTER TABLE `enrolment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `exams`
+--
+ALTER TABLE `exams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exam_questions`
+--
+ALTER TABLE `exam_questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `marks`
+--
+ALTER TABLE `marks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `student_answers`
+--
+ALTER TABLE `student_answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacher_courses`
+--
+ALTER TABLE `teacher_courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attempts`
+--
+ALTER TABLE `attempts`
+  ADD CONSTRAINT `attempts_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `attempts_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `choices`
+--
+ALTER TABLE `choices`
+  ADD CONSTRAINT `choices_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `fk_active_exam` FOREIGN KEY (`next_exam_id`) REFERENCES `exams` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `enrolment`
+--
+ALTER TABLE `enrolment`
+  ADD CONSTRAINT `enrolment_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `enrolment_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `enrolment_ibfk_3` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `exams`
+--
+ALTER TABLE `exams`
+  ADD CONSTRAINT `exams_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `exams_ibfk_2` FOREIGN KEY (`courses_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `exam_questions`
+--
+ALTER TABLE `exam_questions`
+  ADD CONSTRAINT `exam_questions_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `exam_questions_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `marks`
+--
+ALTER TABLE `marks`
+  ADD CONSTRAINT `marks_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `marks_ibfk_2` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `questions_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `student_answers`
+--
+ALTER TABLE `student_answers`
+  ADD CONSTRAINT `student_answers_ibfk_1` FOREIGN KEY (`exam_id`,`student_id`) REFERENCES `attempts` (`exam_id`, `student_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `student_answers_ibfk_2` FOREIGN KEY (`exam_id`,`question_id`) REFERENCES `exam_questions` (`exam_id`, `question_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `student_answers_ibfk_3` FOREIGN KEY (`selected_choice_id`,`question_id`) REFERENCES `choices` (`id`, `question_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `teacher_courses`
+--
+ALTER TABLE `teacher_courses`
+  ADD CONSTRAINT `teacher_courses_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `teacher_courses_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `teacher_courses_ibfk_3` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
